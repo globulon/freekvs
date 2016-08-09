8503ea0c21b7f1cc72a8fa5997f7fd6450e0283f
Marc-Daniel Ortega <globulon@gmail.com>
2016-08-09 23:44:52 +0200

    [FEAT] try to map storage behaviors

--- src/main/scala/utilities/cache/KVSImplementation.scala
+++ src/main/scala/utilities/cache/KVSImplementation.scala
@@ -1,22 +1,20 @@
 package utilities.cache
 
-import redis.RedisCommands
+import redis.{ByteStringDeserializer, ByteStringSerializer, RedisCommands}
 
 import scala.concurrent.Future.fromTry
 import scala.concurrent.{ExecutionContext, Future}
 import scala.language.{higherKinds, reflectiveCalls}
 import scala.util.Try
-import scalaz.Scalaz._
 import scalaz._
 
 trait CanBeStored[+T, +C <: CanBeStored[T, C]] { self: C => }
 
 sealed trait Storage[+K, +V, +C <: CanBeStored[V, C], +S <: Storage[K, V, C, S]] { self: S => }
-
 sealed trait KVS[+K, +V, +C <: CanBeStored[V, C]] extends Storage[K, V, C, KVS[K, V, C]]
 
-case class Get[+K, +V, +C <: CanBeStored[V, C]](k: K) extends KVS[K, V, C]
-case class Put[+K, +V, +C <: CanBeStored[V, C]](k: K, v: V) extends KVS[K, V, C]
+case class Get[+K, +V, +C <: CanBeStored[V, C]](k: K, rule: C) extends KVS[K, V, C]
+case class Put[+K, +V, +C <: CanBeStored[V, C]](k: K, v: V, rule: C) extends KVS[K, V, C]
 
 trait LiftKVS {
   type K
@@ -24,8 +22,8 @@ trait LiftKVS {
   type Store[V] = KVS[K, V, StoreValue[V]]
   type Request[V] = Free[({type L[A] = Coyoneda[Store, A]})#L, V]
 
-  def get[V: StoreValue](k: K): Request[V] = Free.liftFC[Store, V](Get[K, V, StoreValue[V]](k))
-  def put[V: StoreValue](k: K, v: V): Request[V] = Free.liftFC[Store, V](Put[K, V, StoreValue[V]](k, v))
+  def get[V: StoreValue](k: K): Request[V] = Free.liftFC[Store, V](Get[K, V, StoreValue[V]](k, implicitly[StoreValue[V]]))
+  def put[V: StoreValue](k: K, v: V): Request[V] = Free.liftFC[Store, V](Put[K, V, StoreValue[V]](k, v, implicitly[StoreValue[V]]))
 }
 
 trait KVSImplementation {
@@ -33,39 +31,48 @@ trait KVSImplementation {
 
   type Implementation[T]
   type Interpreter = Store ~> Implementation
-  type ValueProjection[+V] 
-  type ProjectValue[A] = StoreValue ~> ValueProjection
+  type ValueProjection[V]
+
+  protected def project: StoreValue ~> ValueProjection
 }
 
 trait InMemoryKVS extends KVSImplementation {
   self: LiftKVS =>
   override type Implementation[T] = StateT[Future, Map[K, Any], T]
+  override type ValueProjection[V] = Any => V
+
+  override def project: StoreValue ~> ValueProjection = new (StoreValue ~> ValueProjection) {
+    override def apply[A](fa: StoreValue[A]): (Any) => A = _.asInstanceOf[A]
+  }
 
   def toMemory(implicit ctx: ExecutionContext): Interpreter = new (Store ~> Implementation) {
     override def apply[A](fa: Store[A]): Implementation[A] = fa match {
-      case Get(k) => StateT[Future, Map[K, Any], A] { m => fromTry(Try((m, m(k).asInstanceOf[A]))) }
-      case Put(k, v) => StateT[Future, Map[K, Any], A] { m => fromTry(Try((m + (k -> v), v))) }
+      case Get(k, rule)    => StateT[Future, Map[K, Any], A] { m => fromTry(Try((m, project(rule)(m(k))))) }
+      case Put(k, v, rule) => StateT[Future, Map[K, Any], A] { m => fromTry(Try((m + (k -> v), v))) }
     }
   }
 }
 
-//trait RedisStorage {
-//  self: LiftStore =>
-//  type K = String
-//  type Persistent[T] = StateT[Future, RedisCommands, T]
-//
-//  def toRedis(implicit ctx: ExecutionContext) : (Store ~> Persistent) = new (Store ~> Persistent) {
-//   override def apply[A](fa: Store[A]): Persistent[A] = fa match {
-//     case Get(k)    => StateT[Future, RedisCommands, A] { c => (c, c.get[A](k)) }
-//     case Put(k, v) => StateT[Future, RedisCommands, A] { c => (c, c.set[A](k, v).map(_ => v)) }
-//   }
-//  }
-//}
+trait RedisStorage extends KVSImplementation {
+  self: LiftKVS =>
+  type K = String
+  override type Implementation[T] = StateT[Future, RedisCommands, T]
+  override type ValueProjection[V] = {
+    def deserializer: ByteStringDeserializer[V]
+    def serializer: ByteStringSerializer[V]
+  }
 
-import scala.concurrent.ExecutionContext.Implicits.global
+  override def project: StoreValue ~> ValueProjection
+
+  def toRedis(implicit ctx: ExecutionContext) : (Store ~> Implementation) = new (Store ~> Implementation) {
+   override def apply[A](fa: Store[A]): Implementation[A] = fa match {
+     case Get(k, rule)    => StateT[Future, RedisCommands, A] { c => c.get[A](k)(project(rule).deserializer).map(_.get).map((c, _)) }
+     case Put(k, v, rule) => StateT[Future, RedisCommands, A] { c => c.set[A](k, v)(project(rule).serializer).map(_ => (c, v)) }
+   }
+  }
+}
 
 trait Storables {
-
   sealed trait StorePrimitive[+T] extends CanBeStored[T, StorePrimitive[T]]
   object StorePrimitive {
     def apply[A](): StorePrimitive[A] = new StorePrimitive[A] {}
@@ -76,7 +83,7 @@ trait Storables {
 
 //object Main extends LiftKVS with InMemoryKVS with Storables {
 //  type K = String
-//  override type Storable[T] = StorePrimitive[T]
+//  override type StoreValue[T] = StorePrimitive[T]
 //
 //  def prg(k: String) = for {
 //    _ <- put[Long](k, 7L)
